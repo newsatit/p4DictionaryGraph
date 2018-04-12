ccce718cf7931156028aace1926ebc9cefe997e0
benschulman <bgschulman31@gmail.com>
2018-04-12 16:10:45 -0500

    implemented addEdge and addVertex

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,4 @@
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,6 +14,14 @@ import java.util.Set;
  */
 public class Graph<E> implements GraphADT<E> {
 
+    private ArrayList<ArrayList<Boolean>> matrix;
+    private ArrayList<E> vertices;
+
+    public Graph(){
+        matrix = new ArrayList<ArrayList<Boolean>>();
+        vertices = new ArrayList<E>();
+    }
+
     /**
      * Instance variables and constructors
      */
@@ -22,8 +31,19 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public E addVertex(E vertex) {
+        if(vertex.equals(null))
+            return null;
+        if(isInGraph(vertex))
+            return null;
 
+        vertices.add(vertex);
+        matrix.add(new ArrayList<Boolean>());
+        matrix.get(matrix.size()-1).add(new Boolean(false));
+
+        return vertex;
     }
+
+
 
     /**
      * {@inheritDoc}
@@ -38,6 +58,20 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
+        if(vertex1.equals(null) || vertex2.equals(null))
+            return false;
+        if(vertex1.equals(vertex2))
+            return false;
+
+        int index1 = vertices.indexOf(vertex1);
+        int index2 = vertices.indexOf(vertex2);
+
+        if(index1 == -1 || index2 == -1)
+            return false;
+
+        matrix.get(index1).set(index2, true);
+        matrix.get(index2).set(index1, true);
+
 
     }
 
@@ -71,6 +105,13 @@ public class Graph<E> implements GraphADT<E> {
     @Override
     public Iterable<E> getAllVertices() {
 
+    }
+
+    private boolean isInGraph(E vertex) {
+        for(int i = 0; i<vertices.size(); i++)
+            if(vertices.get(i).equals(vertex))
+                return true;
+        return false;
     }
 
 }
