fa89c326160ac55c54b7d2bda986da087a81c35b
benschulman <bgschulman31@gmail.com>
2018-04-14 19:57:53 -0500

    commented the graph class

--- src/Graph.java
+++ src/Graph.java
@@ -30,6 +30,7 @@ public class Graph<E> implements GraphADT<E> {
     //keeps track of vertices
     private ArrayList<E> vertices;
 
+    // Initializes the adjacency matrix and list of vertices
     public Graph(){
         matrix = new ArrayList<ArrayList<Boolean>>();
         vertices = new ArrayList<E>();
@@ -46,32 +47,38 @@ public class Graph<E> implements GraphADT<E> {
     public E addVertex(E vertex) {
         if(vertex == null)
             return null;
+        // if the vertex is already in the graph then return null
         if(isInGraph(vertex))
             return null;
 
+        // if we are adding the first vertex we have special conditions
         if(vertices.size() == 0){
-            vertices.add(vertex);
-            matrix.add(new ArrayList<Boolean>());
-            matrix.get(0).add(new Boolean(false));
+            vertices.add(vertex); // add the vertex to the list
+            matrix.add(new ArrayList<Boolean>()); // add the first row to the matrix
+            matrix.get(0).add(new Boolean(false)); // add the first "column" and assign the boolean value to false
             return vertex;
         }
-        //add the element to the graph
+        // If it is not the first vertex we do the following
+
+        //add the vertex to the list
         vertices.add(vertex);
-        //add a new row to the adjacency matrix and set the new values equal to false
+        //add a new row to the adjacency matrix
         matrix.add(new ArrayList<Boolean>());
 
-        // adding existing columns to the new row
+        // add existing columns to the new row to keep the matrix square
         for(int j = 0; j < matrix.get(0).size(); j++){
+            // set the new values to false because there is not an edge
             matrix.get(matrix.size()-1).add(new Boolean(false));
         }
 
-        // add new column to all rows
+        // add one new column to all rows
         for(int i = 0; i<matrix.size();i++){
+            // set the initial values to false because there is no edge
             matrix.get(i).add(new Boolean(false));
         }
 
-        //printMatrix();
 
+        // return the vertex that was added
         return vertex;
     }
 
@@ -85,16 +92,21 @@ public class Graph<E> implements GraphADT<E> {
         if(vertex == null)
             return null;
 
+        // get the index of the vertex in the list
         int index = vertices.indexOf(vertex);
 
+        // if the vertex is not in the graph return null
         if(index == -1)
             return null;
-        //printMatrix();
-        //remove from all places
-        vertices.remove(index);
-        //matrix.get(index).remove(index);
 
+        //remove from all places
+
+        //remove the vertex from the list
+        vertices.remove(index);
+
+        // remove the row vertex
         matrix.remove(index);
+        // remove the columns from each row
         for(int i = 0; i<matrix.size(); i++){
             matrix.get(i).remove(index);
         }
@@ -117,6 +129,7 @@ public class Graph<E> implements GraphADT<E> {
         int index1 = vertices.indexOf(vertex1);
         int index2 = vertices.indexOf(vertex2);
 
+        // if either are not in the graph return false
         if(index1 == -1 || index2 == -1)
             return false;
 
@@ -142,6 +155,7 @@ public class Graph<E> implements GraphADT<E> {
         int index1 = vertices.indexOf(vertex1);
         int index2 = vertices.indexOf(vertex2);
 
+        // if either of the vertices are not in the graph return null
         if(index1 == -1 || index2 == -1)
             return false;
 
@@ -162,13 +176,15 @@ public class Graph<E> implements GraphADT<E> {
         if(vertex1.equals(vertex2))
             return false;
 
+        // find the vertices
         int index1 = vertices.indexOf(vertex1);
         int index2 = vertices.indexOf(vertex2);
 
+        // if they are not in the graph return false
         if(index1 == -1 || index2 == -1)
             return false;
 
-        // searches the matrix to see if the location has a true
+        // returns whatever is stored in the adjacency matrix
         return matrix.get(index1).get(index2);
 
     }
@@ -181,11 +197,14 @@ public class Graph<E> implements GraphADT<E> {
         if(vertex == null)
             return null;
 
+        // get the index of the vertex
         int index = vertices.indexOf(vertex);
 
+        // return null if the vertex is not in the graph
         if(index == -1)
             return null;
 
+        // create a new list that stores neighbors
         ArrayList<E> neighbors = new ArrayList<E>();
 
         //loops through the matrix and adds to the list if there is an edge
@@ -204,16 +223,26 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getAllVertices() {
-        return vertices;
+        return vertices; // returns the list of vertices
     }
 
+    /**
+     * Checks if a vertex is in a graph
+     *
+     * @param vertex the vertex to check
+     * @return true if vertex is in the graph
+     */
     private boolean isInGraph(E vertex) {
+        //Loops through the vertex array to find vertex
         for(int i = 0; i<vertices.size(); i++)
-            if(vertices.get(i).equals(vertex))
+            if(vertices.get(i).equals(vertex)) // returns true if it is found
                 return true;
         return false;
     }
 
+    /**
+     * Debug method to help. Prints the adjacency matrix
+     */
     private void printMatrix(){
         for(int i =0; i<matrix.size(); i++){
             for(int j =0; j<matrix.get(i).size(); j++){
